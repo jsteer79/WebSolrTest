5c227fe34b051ad53ea81176e98bc27d3201c358
James Steer <j.steer@sthree.com>
2015-10-01 17:18:53 +0100

    Add Query Route to test max post size

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ var app = express();
 app.use( express.json() )
    .use( express.urlencoded() );
 
-app.get( '/'       , routes.count );
-app.get( '/add'    , routes.add );
-app.get( '/clear'  , routes.clear );
-app.get('/del/:key', routes.del );
+app.get( '/'        	, routes.count );
+app.get( '/add'     	, routes.add   );
+app.get( '/clear'   	, routes.clear );
+app.get( '/del/:key'	, routes.del   );
+app.get( '/query/:size'	, routes.query );
 
 app.listen( nconf.get('PORT') );
